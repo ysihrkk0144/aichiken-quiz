65ab05d76a1ec7233bb529e60ba4664ae807a3ad
ysihrkk0144 <ucchi.micchi.190@gmail.com>
2026-06-07 22:29:28 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+var CACHE_NAME = 'aichi-quiz-v1';
+var ASSETS = [
+  './aichikenquiz.html',
+  './manifest.json',
+  './service-worker.js',
+  './aichi.topojson',
+  './icon.svg'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k)   { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      if (cached) return cached;
+      return fetch(event.request).then(function(response) {
+        if (response && response.status === 200 && response.type === 'basic') {
+          var clone = response.clone();
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, clone);
+          });
+        }
+        return response;
+      }).catch(function() { return cached; });
+    })
+  );
+});
